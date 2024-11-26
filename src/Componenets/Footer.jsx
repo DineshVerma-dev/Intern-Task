0045004dd47da04a457205124490d5a2166405ea
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-11-26 21:42:52 +0530

    done with edit

--- src/Componenets/Footer.jsx
+++ src/Componenets/Footer.jsx
@@ -2,36 +2,30 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900  p-28  text-white py-8">
-      <div className="max-w-screen-xl m-10 mx-auto flex justify-between items-center px-6">
-       
-        <div className="flex items-center">
-          <img
-            src="/moonex.png" 
-            alt="Logo"
-            className="h-20"
-          />
-        </div>
-
-      
-        <div className="flex space-x-6 text-xl font-fira">
-          <a href="#" className="hover:text-gray-400">Home</a>
-          <a href="#" className="hover:text-gray-400">About</a>
-          <a href="#" className="hover:text-gray-400">Services</a>
-          <a href="#" className="hover:text-gray-400">Contact</a>
-        </div>
-
-
-        <div className="flex space-x-4 text-xl">
-          <div>
-            Contact Us
+    <footer className="bg-gray-900 text-white py-8">
+      <div className="max-w-screen-xl mx-auto px-6">
+        <div className="flex flex-col sm:flex-row justify-between items-center space-y-6 sm:space-y-0 sm:space-x-6">
+         
+        
+          <div className="flex justify-center sm:justify-start">
+            <img
+              src="/moonex.png" 
+              alt="Logo"
+              className="h-20"
+            />
           </div>
 
 
+          <div className="hidden sm:flex flex-col sm:flex-row space-y-4 sm:space-y-0 text-xl font-fira sm:space-x-6 justify-center sm:justify-start">
+            <a href="#" className="hover:text-gray-400">Home</a>
+            <a href="#" className="hover:text-gray-400">About</a>
+            <a href="#" className="hover:text-gray-400">Services</a>
+          </div>
 
-
-
-
+          
+          <div className="flex justify-center sm:justify-start text-xl">
+            Contact Us
+          </div>
         </div>
       </div>
     </footer>
