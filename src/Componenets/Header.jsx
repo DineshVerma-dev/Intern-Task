4e1583914d431a4e0d9b969ef082679ead61d14c
DINESH VERMA <dineshkumarverma.dev@gmail.com>
2024-11-26 19:30:26 +0530

    add hamberger

--- src/Componenets/Header.jsx
+++ src/Componenets/Header.jsx
@@ -1,50 +1,3 @@
-// import React from "react";
-
-// function Header() {
-//   const navItems = [
-//     { name: "Home" },
-//     { name: "About Us" },
-//     { name: "Roadmap" },
-//     { name: "FAQs" },
-//     { name: "Contact Us" },
-//   ];
-
-//   return (
-//     <>
-//       {/* Header Section */}
-//       <header className="bg-transparent">
-//         <div className="container mx-auto flex justify-evenly items-center py-10 px-6">
-//           {/* Logo */}
-//           <div className="flex items-center text-2xl font-bold">
-//             <img
-//               src="/moonex.png" // Replace with your logo image path
-//               alt="Logo"
-//               className="" // Adjust size and spacing
-//             />
-           
-//           </div>
-
-//           {/* Navigation Menu */}
-//           <ul className="hidden md:flex space-x-10 text-xl">
-//             {navItems.map((item) => (
-//               <li key={item.name}>
-//                 <button className="hover:text-yellow-400 text-white font-fira">{item.name}</button>
-//               </li>
-//             ))}
-//           </ul>
-
-//           {/* Wallet Button */}
-//           <button className="bg-yellow-400 text-black font-bold py-2 px-4 rounded-lg hover:bg-yellow-500">
-//             Connect Wallet
-//           </button>
-//         </div>
-//       </header>
-//     </>
-//   );
-// }
-
-// export default Header;
-
 
 import React, { useState } from "react";
 
@@ -61,13 +14,13 @@ function Header() {
 
   return (
     <header className="bg-transparent w-full ">
-      <div className="container mx-auto flex justify-around items-center py-10  px-6 sm:px-6 md:px-10">
+      <div className="container mx-auto flex justify-between items-center py-10  px-6 sm:px-6 md:px-10">
         {/* Logo */}
         <div className="flex items-center text-2xl font-bold">
           <img
             src="/moonex.png" 
             alt="Logo"
-            className="h-8 sm:h-10"
+            className="h-10 sm:h-16"
           />
         </div>
 
@@ -82,7 +35,7 @@ function Header() {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              className="w-6 h-6"
+              className="w-8 h-8"
             >
               {isMenuOpen ? (
                 <path
